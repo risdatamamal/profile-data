faf2b1ea0465bad494722655dfba1b73c55e2753
Risda Tamam Aljava <risda123tamam@student.esaunggul.ac.id>
2023-09-05 17:23:17 +0700

    create project yii and add docker

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=172.19.0.1:8000;dbname=db_profiledata',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
